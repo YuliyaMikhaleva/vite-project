1c08135ef72cb5fba4d2aa1a7f6748eb6251079a
YuliyaMikhaleva <umihaleva93@mail.ru>
2023-09-26 13:22:32 +0500

    added assets to project

--- TestProject/src/main.tsx
+++ TestProject/src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.scss'
+import './assets/styles/style.scss'
+// import './index.scss'
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
